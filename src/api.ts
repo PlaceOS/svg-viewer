ad3341a2857f26c4250d44d5febf18234e3addba
Alex Sorafumo <alex@yuion.net>
2020-10-26 18:24:08 +1100

    chore: clean up api methods

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,10 @@
 import { BehaviorSubject, Observable } from 'rxjs';
-
-import { Viewer } from "./viewer.class";
-import { HashMap } from './types';
-import { createView, renderView, resizeView } from './renderer';
-import { listenForCustomViewActions } from './input';
-import { unsubscribeWith } from './async';
 import { distinct, filter, map } from 'rxjs/operators';
+
+import { timeout, unsubscribeWith } from './async';
+import { createView, renderView, resizeView } from './renderer';
+import { HashMap } from './types';
+import { Viewer } from './viewer.class';
 
 /**
  * @hidden
@@ -23,7 +22,7 @@ export const _update_timers: HashMap<number> = {};
 export const _svg_cache: HashMap<string> = {};
 
 export function getViewer(id: string): Viewer | undefined {
-    return _svg_viewers.getValue().find(viewer => viewer.id === id);
+    return _svg_viewers.getValue().find((viewer) => viewer.id === id);
 }
 
 /**
@@ -32,8 +31,8 @@ export function getViewer(id: string): Viewer | undefined {
  */
 export function listenToViewerChanges(id: string): Observable<Viewer> {
     return _svg_viewers.pipe(
-        filter(list => !!list.find(viewer => viewer.id === id)),
-        map(list => list.find(viewer => viewer.id === id)),
+        filter((list) => !!list.find((viewer) => viewer.id === id)),
+        map((list) => list.find((viewer) => viewer.id === id)),
         distinct()
     ) as any;
 }
@@ -44,7 +43,7 @@ export function listenToViewerChanges(id: string): Observable<Viewer> {
 export function resizeViewers() {
     const viewer_list = _svg_viewers.getValue();
     for (const viewer of viewer_list) {
-        resizeView(viewer);
+        timeout(`resize-${viewer.id}`, () => resizeView(viewer));
     }
 }
 
@@ -54,11 +53,11 @@ export function resizeViewers() {
  */
 export async function createViewer(options: Partial<Viewer>) {
     const view_list = _svg_viewers.getValue();
-    let viewer = view_list.find(viewer => viewer.url === options.url);
+    let viewer = view_list.find((v) => v.url === options.url);
     if (viewer) {
         return viewer.id;
     }
-    const svg_data = options.svg_data || await loadSVGData(options.url);
+    const svg_data = options.svg_data || (await loadSVGData(options.url));
     viewer = new Viewer({ ...options, svg_data });
     _svg_viewers.next([...view_list, viewer]);
     createView(viewer);
@@ -70,24 +69,25 @@ export async function createViewer(options: Partial<Viewer>) {
  * @param viewer Viewer or ID to update
  * @param options New details for the viewer
  */
-export function updateViewer(viewer: string, options: Partial<Viewer>, render?: boolean): Viewer;
-export function updateViewer(viewer: Viewer, options: Partial<Viewer>, render?: boolean): Viewer;
-export function updateViewer(viewer: string | Viewer, options: Partial<Viewer>, render: boolean = true): Viewer {
+export function updateViewer(
+    viewer: string | Viewer,
+    options: Partial<Viewer>,
+    render: boolean = true
+): Viewer {
     const view_list = _svg_viewers.getValue();
-    if (!(viewer instanceof Viewer)) { viewer = view_list.find(v => v.id === viewer)!; }
+    viewer = view_list.find((v) => v.id === (viewer instanceof Viewer ? viewer.id : viewer))!;
     if (!(viewer instanceof Viewer)) throw new Error('Unable to find viewer');
     delete options.url;
     const updated_viewer = new Viewer({ ...(viewer as Viewer), ...options });
-    _svg_viewers.next(view_list.filter(v => v.id !== updated_viewer.id).concat([updated_viewer]));
+    _svg_viewers.next(view_list.filter((v) => v.id !== updated_viewer.id).concat([updated_viewer]));
     if (updated_viewer.needs_update) {
         if (_update_timers[viewer.id]) {
             clearTimeout(_update_timers[viewer.id]);
         }
-        _update_timers[viewer.id] = <any>setTimeout(() => updateViewer(updated_viewer, {}), 16);
+        _update_timers[viewer.id] = setTimeout(() => updateViewer(updated_viewer, {}), 16) as any;
     }
     if (render) {
         renderView(updated_viewer);
-        listenForCustomViewActions(updated_viewer);
     }
     return updated_viewer;
 }
@@ -105,7 +105,7 @@ export function removeViewer(id: string) {
     const viewer_list = _svg_viewers.getValue();
     // Remove listeners for viewer
     unsubscribeWith(`${id}`);
-    _svg_viewers.next(viewer_list.filter(v => v.id !== id));
+    _svg_viewers.next(viewer_list.filter((v) => v.id !== id));
 }
 
 /**
@@ -114,9 +114,7 @@ export function removeViewer(id: string) {
  * @param url URL to load SVG data for
  */
 export async function loadSVGData(url: string = '') {
-    const resp = _svg_cache[url]
-        ? { text: async () => _svg_cache[url] }
-        : await fetch(url);
+    const resp = _svg_cache[url] ? { text: async () => _svg_cache[url] } : await fetch(url);
     const text = await resp.text();
     _svg_cache[url] = text;
     return text;
